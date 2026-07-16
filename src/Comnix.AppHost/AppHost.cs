057207984f6cf1e09a6c5df43d35bbfccd6e7b51
Luiz Adolfo <luiz-adolfo@hotmail.com>
2026-07-16 09:00:06 -0300

    Implemented aspire orchestration

--- src/Comnix.AppHost/AppHost.cs
+++ src/Comnix.AppHost/AppHost.cs
@@ -1,3 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var sshServer = builder.AddContainer("ssh-server", "lscr.io/linuxserver/openssh-server", "latest")
+    .WithEndpoint(name: "ssh", port: 2222, targetPort: 2222)
+    .WithEnvironment("PUID", "1000")
+    .WithEnvironment("PGID", "1000")
+    .WithEnvironment("USER_NAME", "test")
+    .WithEnvironment("USER_PASSWORD", "pass123")
+    .WithEnvironment("PASSWORD_ACCESS", "true");
+
+builder.AddDockerfile(name: "comnix", contextPath: "../../", dockerfilePath: "src/Comnix/Dockerfile")
+    .WithHttpEndpoint(port: 5000, targetPort: 5000)
+    .WithBindMount("volumes/comnix/config", "/app/config")
+    .WithImage("comnix:latest")
+    .WaitFor(sshServer);
+
 await builder.Build().RunAsync();
